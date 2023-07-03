d872b039865e077ad473e5420177b9086d461a7e
Digvijay Singh <digvijayrn555@gmail.com>
2023-07-03 10:31:05 +0530

    Initial commit

---
+++ src/main/java/com/supercompany/seonotes/SeonotesApplication.java
@@ -0,0 +1,13 @@
+package com.supercompany.seonotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeonotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeonotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supercompany/seonotes/SeonotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supercompany.seonotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeonotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
